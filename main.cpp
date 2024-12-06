76a9f1e3262719038f203c6b0fcc14cfa34e08d9
sasimohammed <144622733+sasimohammed@users.noreply.github.com>
2024-12-06 16:18:06 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,87 +1,139 @@
 #include <iostream>
-#include "four_in_row_board.h"
-#include "numerical_tic_tac_toe_board.h"
-#include "Tic_tac_toe5_board.h"
-#include "game6_tic_tac_toe.h"
-#include "Ultimate Tic Tac Toe .h"
-
+#include "BoardGame_Classes.h"
+#include "3x3X_O.h"
+#include "MinMaxPlayer.h"
+#include "Four-in-a-row.h"
+#include "Numerical Tic-Tac-Toe.h"
 using namespace std;
 
 int main() {
-    cout << "Hello in our board games" << endl;
-    cout << "Choose your game:" << endl;
-    cout << "1) Four in a Row" << endl;
-    cout << "2) Numerical Tic Tac Toe" << endl;
-    cout << "3) 5x5 Tic Tac Toe" << endl;
-    cout << "4) Misere Tic Tac Toe" << endl;
-    cout << "5) Ultimate Tic Tac Toe" << endl;
+    cout << "Welcome to FCAI  Games. :)\n";
+    cout<<"1) four_in_row_game"<<endl;
+    cout<<"2) Numerical_Tic_Tac_Toe_board"<<endl;
+    cout<<"-------------------choose your game to start----------------------"<<endl;
+    int gamenum;
+    cin>>gamenum;
+    if(gamenum==1){
+        int choice;
+        Player<char>* players[2];
+        four_in_row_board<char>*B = new  four_in_row_board<char>();
+        string playerXName, player2Name;
+        cout << "Enter Player 1 name: ";
+        cin >> playerXName;
+        cout << "Choose Player 1 type:\n";
+        cout << "1. Human\n";
+        cout << "2. Random Computer\n";
+        cout << "3. Smart Computer (AI)\n";
+        cin >> choice;
 
-    int choice;
-    cin >> choice;
+        switch(choice) {
+            case 1:
+                players[0] = new  four_in_row_Player<char>(playerXName, 'X');
+                break;
+            case 2:
+                players[0] = new four_in_row_Random_Player<char>('X');
+                break;
+            case 3:
+                players[0] = new X_O_MinMax_Player<char>('X');
+                players[0]->setBoard(B);
+                break;
+            default:
+                cout << "Invalid choice for Player 1. Exiting the game.\n";
+                return 1;
+        }
+        // Set up player 2
+        cout << "Enter Player 2 name: ";
+        cin >> player2Name;
+        cout << "Choose Player 2 type:\n";
+        cout << "1. Human\n";
+        cout << "2. Random Computer\n";
+        cout << "3. Smart Computer (AI)\n";
+        cin >> choice;
 
-    if (choice == 1) {
-        cout << "Player 1: X" << endl;
-        cout << "Player 2: O" << endl;
-        four_in_row_board game_board(7, 6, '-');
-        game_board.display();
-        vector<char> player1_s = {'X'};
-        vector<char> player2_s = {'O'};
-        player<vector<char>> player1(player1_s);
-        player<vector<char>> player2(player2_s);
-        four_in_row_manage game_manager(game_board, player1, player2);
-        game_manager.take_turn();
+        switch(choice) {
+            case 1:
+                players[1] = new four_in_row_Player<char>(player2Name, 'O');
+                break;
+            case 2:
+                players[1] = new X_O_Random_Player<char>('O');
+                break;
+            case 3:
+                players[1] = new X_O_MinMax_Player<char>('O');
+                players[1]->setBoard(B);
+                break;
+            default:
+                cout << "Invalid choice for Player 2. Exiting the game.\n";
+                return 1;
+        }
+        GameManager<char> four_in_row(B, players);
+        four_in_row.run();
+        delete B;
+        for (int i = 0; i < 2; ++i) {
+            delete players[i];
+        }
     }
-    else if (choice == 2) {
-        numirecal_tic_tac_toe_board game_board(3, 3, 45);
-        game_board.display();
-        cout << "Player 1 numbers: 1, 3, 5, 7" << endl;
-        vector<int> player1_numbers = {1, 3, 5, 7};
-        player<vector<int>> player1(player1_numbers);
-        cout << "Player 2 numbers: 2, 4, 6, 8" << endl;
-        vector<int> player2_numbers = {2, 4, 6, 8};
-        player<vector<int>> player2(player2_numbers);
-        numirecal_tic_tac_toe_manage game_manager(game_board, player1, player2);
-        game_manager.take_turn();
+    else if(gamenum==2){
+        int choice;
+        Player<char>* players[2];
+        Numerical_Tic_Tac_Toe_board<char>*B = new  Numerical_Tic_Tac_Toe_board<char>();
+        string playerXName, player2Name;
+
+        cout << "Enter Player 1 name: ";
+        cin >> playerXName;
+        cout << "Choose Player 1 type:\n";
+        cout << "1. Human\n";
+        cout << "2. Random Computer\n";
+        cout << "3. Smart Computer (AI)\n";
+        cin >> choice;
+        switch(choice) {
+            case 1:
+                players[0] = new  Numerical_Tic_Tac_Toe_Player<char>(playerXName, '0');
+                break;
+            case 2:
+
+                players[0] = new Numerical_Tic_Tac_Toe_Random_Player<char>('0');
+
+                break;
+            case 3:
+
+            default:
+                cout << "Invalid choice for Player 1. Exiting the game.\n";
+                return 1;
+        }
+        // Set up player 2
+        cout << "Enter Player 2 name: ";
+        cin >> player2Name;
+        cout << "Choose Player 2 type:\n";
+        cout << "1. Human\n";
+        cout << "2. Random Computer\n";
+        cout << "3. Smart Computer (AI)\n";
+        cin >> choice;
+
+        switch(choice) {
+            case 1:
+
+                players[1] = new Numerical_Tic_Tac_Toe_Player<char>(player2Name, '0');
+                break;
+            case 2:
+                players[1] = new Numerical_Tic_Tac_Toe_Random_Player<char>('0');
+                break;
+            case 3:
+
+            default:
+                cout << "Invalid choice for Player 2. Exiting the game.\n";
+                return 1;
+        }
+        GameManager<char>  Numerical_Tic_Tac_Toe(B, players);
+        Numerical_Tic_Tac_Toe.run();
+        delete B;
+        for (int i = 0; i < 2; ++i) {
+            delete players[i];
+        }
     }
-    else if (choice == 3) {
-        cout << "Player 1: X" << endl;
-        cout << "Player 2: O" << endl;
-        game5 game_board(5, 5, '-');
-        game_board.display();
-        vector<char> player1_s = {'X'};
-        vector<char> player2_s = {'O'};
-        player<vector<char>> player1(player1_s);
-        player<vector<char>> player2(player2_s);
-        manage_game5 game_manager(game_board, player1, player2);
-        game_manager.take_turn();
-    }
-    else if (choice == 4) {
-        cout << "Player 1: X" << endl;
-        cout << "Player 2: O" << endl;
-        game6 game_board(3, 3, '-');
-        game_board.display();
-        vector<char> player1_s = {'X'};
-        vector<char> player2_s = {'O'};
-        player<vector<char>> player1(player1_s);
-        player<vector<char>> player2(player2_s);
-        manage_game6 game_manager(game_board, player1, player2);
-        game_manager.take_turn();
-    }
-    else if (choice == 5) {
-        cout << "player1: X" << endl;
-        cout << "player2: O" << endl;
-        Ultimate_Tic_Tac_Toe_board main_board(3, 3, '-');
-        Ultimate_Tic_Tac_Toe_board game_board(3, 3, '-');
-        vector<char> player1_s = {'X'};
-        vector<char> player2_s = {'O'};
-        player<vector<char>> player1(player1_s);
-        player<vector<char>> player2(player2_s);
-        Ultimate_Tic_Tac_Toe_manage game_board_manager(game_board, player1, player2);
-        game_board_manager.take_turn();
-    }
-    else {
-        cout << "Invalid choice! Please select a valid game option." << endl;
-    }
+
+
 
     return 0;
 }
+
+
